adbca6eb7804654f909e2107241508e6c11ca48f
Adal1942 <adautosombra1942@gmail.com>
2022-04-20 14:10:35 -0300

    teste 4

---
+++ js/setup-inicio.js
@@ -0,0 +1,54 @@
+$('.sajid-1').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+});
+$('.sajid-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    responsive:{
+        0:{
+            items:4
+        },
+        700:{
+            items:4
+        },
+        1000:{
+            items:10
+        }
+    }
+});
+$('.sajid-3').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        760:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    }
+});
